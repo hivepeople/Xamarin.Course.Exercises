4019bd938e3e4f27b74311b7d001373dd00aa615
bvi@hivepeople.dk <snb@hivepeople.dk>
2016-11-14 14:58:40 +0100

    Added our live code for collections and value types vs reference types

---
+++ Expressions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int x = 32;
+            List<int> evenNumbers = new List<int>();
+            ArrayList old = new ArrayList();
+            Dictionary<int, bool> isEvenNumberDict = new Dictionary<int, bool>();
+
+            for (int i = 0; i <= x; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNumbers.Add(i);
+                    old.Add(i);
+                    isEvenNumberDict.Add(i, true);
+                }
+                else
+                {
+                    isEvenNumberDict.Add(i, false);
+                }
+            }
+
+            foreach (int number in evenNumbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            foreach (var entry in isEvenNumberDict)
+            {
+                Console.WriteLine(entry.Key + " = " + entry.Value);
+            }
+
+            Console.WriteLine("First item on list: " + evenNumbers[0]);
+            Console.WriteLine("Is 3 an even number? " + isEvenNumberDict[3]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ValueTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNumberClass classVal = new MyNumberClass();
+            classVal.value = 3;
+
+            MyNumberStruct structVal = new MyNumberStruct();
+            structVal.value = 3;
+
+            AddTo(classVal, 1);
+            AddTo(structVal, 1);
+
+            Console.WriteLine("AddTo class = " + classVal);
+            Console.WriteLine("AddTo struct = " + structVal);
+            Console.ReadKey();
+        }
+
+        static void AddTo(MyNumberClass x, int y)
+        {
+            x.value += y;
+        }
+
+        static void AddTo(MyNumberStruct x, int y)
+        {
+            x.value += y;
+        }
+    }
+
+    public class MyNumberClass
+    {
+        public int value;
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+
+    public struct MyNumberStruct
+    {
+        public int value;
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+}
