238a39f758ce1bccbda28d69df0bb81319cb1ff1
bvi@hivepeople.dk <snb@hivepeople.dk>
2016-11-17 15:16:28 +0100

    Added some simulated work

--- AsyncWpfExample/MainWindow.xaml.cs
+++ AsyncWpfExample/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -42,12 +43,18 @@ namespace AsyncWpfExample
         {
             slowWorkProgressLabel.Content = "Working...";
 
+            // Simulate doing work for 10 seconds
+            Thread.Sleep(10000);
+
             slowWorkProgressLabel.Content = "Done!";
         }
 
-        private void slowWorkAsyncButton_Click(object sender, RoutedEventArgs e)
+        private async void slowWorkAsyncButton_Click(object sender, RoutedEventArgs e)
         {
             slowWorkAsyncProgressLabel.Content = "Working...";
+
+            // Simulate doing work for 10 seconds
+            await Task.Delay(10000);
 
             slowWorkAsyncProgressLabel.Content = "Done!";
         }
